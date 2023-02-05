7be223a13165a73162652703b1a9937ce539b8e0
leechulhan <dhfhfkwhdk@naver.com>
2023-02-05 19:23:35 +0900

    Test 시 depracated assertThat -> assertTrue 로 수정

--- springboot-webservice/webservice/src/test/java/com/springboot/webservice/domain/posts/PostRepositoryTest.java
+++ springboot-webservice/webservice/src/test/java/com/springboot/webservice/domain/posts/PostRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.springboot.webservice.domain.posts;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
@@ -30,7 +29,7 @@ public class PostRepositoryTest {
 			postRepository.deleteAll();
 		}
 		
-		// @Test
+		@Test
 		public void 게시글저장_불러오기() {
 			// given - (테스트 기반 환경을 구축하는 단계, 여기선 @Builder 의 사용법도 같이 확인)
 			postRepository.save(Posts.builder()
@@ -45,15 +44,18 @@ public class PostRepositoryTest {
 			// then - 테스트 결과 검증, 실제로 DB 에 insert 되었는 지 확인하기 위해 조회 후 입력된 값 확인
 			Posts posts = postsList.get(0);
 			
+			assertTrue(posts.getTitle().equals("테스트 게시글"));
+			assertTrue(posts.getAuthor().equals("Springboot Test"));
+			assertTrue(posts.getContent().equals("테스트 본문"));
 			
-			assertThat(posts.getTitle(), is("테스트 게시글"));
+			// assertThat 은 depracated 가 됨
 			
-			assertThat(posts.getTitle(), is("테스트 게시글"));
-			assertThat(posts.getContent(), is("테스트 본문"));
+//			assertThat(posts.getTitle(), is("테스트 게시글"));
+//			assertThat(posts.getTitle(), is("테스트 게시글"));
+//			assertThat(posts.getContent(), is("테스트 본문"));
 			
 			// Given When Then 은 BDD (Behaviour-Driven-Development) 에서 사용 하는 용어임
 			// JUnit 에서는 이를 명시적으로 지원해주지 않아 주석으로 표현
-			
 			// 전문 BDD 프레임워크로 Groovy 기반의 Spock 를 많이 사용
 		}
 		
