b0ef4e1b86e3dfba990212f36a42e6264c5d7ea5
leechulhan <dhfhfkwhdk@naver.com>
2023-02-12 19:52:45 +0900

    Property profile 방식 변경

--- springboot-webservice/webservice/src/main/java/com/springboot/webservice/WebserviceApplication.java
+++ springboot-webservice/webservice/src/main/java/com/springboot/webservice/WebserviceApplication.java
@@ -1,23 +1,42 @@
 package com.springboot.webservice;
 
-import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import lombok.AllArgsConstructor;
 
 @EnableJpaAuditing		// JPA Auditing 활성화
 @SpringBootApplication
+@AllArgsConstructor
 public class WebserviceApplication {
 
-	 public static final String APPLICATION_LOCATIONS = "spring.config.location="
-	            + "classpath:application.properties,"
-	            + "/home/app/config/webservice/prod-application.yml";
+	private static final Logger logger = LoggerFactory.getLogger(WebserviceApplication.class);
 	
-	 public static void main(String[] args) {
-		 new SpringApplicationBuilder(WebserviceApplication.class) 
-		 		.profiles(APPLICATION_LOCATIONS)
-		 		.run(args);
-		// SpringApplication.run(WebserviceApplication.class, args);
+	public static final String APPLICATION_LOCATIONS = // "spring.config.additional-location="
+			 "spring.config.location="
+			 + "classpath:application.properties,"
+	         + "classpath:application-test.yml,";
+	 		 // + "file:C:///Users/dhfhf/Desktop/application-ext.properties";
+	/*
+	 * mvn spring-boot:run
+	 * -Dspring.config.location="file:///Users/home/jdbc.properties" mvn
+	 * spring-boot:run
+	 * -Dspring.config.location="file:///D:/config/aliyun-oss-java/application.yml"
+	 */
+	
+	
+	public static void main(String[] args) {
+		 // logger.info("TEST ACTIVE PROFILE TEST : {} ", System.getProperty("spring.profiles.active"));
+//		 new SpringApplicationBuilder(WebserviceApplication.class) 
+//		 		.profiles(APPLICATION_LOCATIONS)
+//		 		.run(args);
+//		 
+//		 logger.info("TEST ACTIVE PROFILE TEST : {} ", System.getProperty("spring.profiles.active"));
+		
+		SpringApplication.run(WebserviceApplication.class, args);
 	}
 
 }
